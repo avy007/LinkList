fe6f59fb8537cd83c8e1f37d9fb3ff0982a10861
akash <akashyewale007@gmail.com>
2023-03-11 06:40:45 +0530

    Add First

--- src/com/linklist/linklist.java
+++ src/com/linklist/linklist.java
@@ -1,85 +1,64 @@
 package com.linklist;
-
-
 public class linklist {
-	
-	
+		
 Node head;
 	
-	class Node{
-		
+	class Node{		
 		Object data;
 		Node next;
 		
 		Node (Object data){
 			this.data=data;
-			this.next=null;
-			
+			this.next=null;		
 		}
 	}
 	// add first
 	  public void addFirst(Object data){
 
 		    Node newNode = new Node(data);
-
 		    if(head == null){
-
 		    head = newNode;
-
 		    return;
-
 		   }
-
 		   newNode.next = head;
-
 		   head = newNode;
-
 		   }
+	  
 	  //print
 		   public void printNode(){
-
 		    if(head==null){
-
 		       System.out.println("Linked list is empty...");
-
 		       return;
 		   }
-
 		    Node tempNode = head;
-
 		    while(tempNode != null){
-
 		    System.out.print(tempNode.data + "=>");
-
 		    tempNode = tempNode.next;
-
 		   }
-
 		    System.out.println("null");
-
-		   }   
-		   // add last
+		   }  
+		   
+      // add last
 		   public void addLast(Object data) {
-
 		       Node newNode = new Node(data);
-
 		        if(head==null){
-
 		           System.out.println("Linked list is empty...");
-
 		           return;
-
 		           }
-
 		        Node tempNode = head;
-
 		        while(tempNode.next != null){
-
 		        tempNode = tempNode.next;
-
 		       }
-
 		        tempNode.next=newNode;    
+		   }
+		   //delete first
+		   public void  deletefirst() {
+			   
+			   if(head==null) {
+				   System.out.println("list is empty");
+				   return;
+			   }	 
+			   head =head.next; 
 		   }
 
 
@@ -88,6 +67,8 @@ Node head;
 		ll.addFirst(30);
 		ll.addFirst(56);
 		ll.addLast(70);
+		ll.printNode();
+		ll.deletefirst();
 		ll.printNode();
 
 	}
